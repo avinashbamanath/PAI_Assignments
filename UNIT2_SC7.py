e1dffdf5564e37dcce92cca8f3d0de7659159ab4
Avinash <avinashbamanath@gmail.com>
2026-09-17 10:13:43 +0530

    Add files via upload

---
+++ UNIT2_SC7.py
@@ -0,0 +1,21 @@
+def lcs_length(str1, str2):
+    m = len(str1)
+    n = len(str2)
+
+    # DP table
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if str1[i - 1] == str2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+    return dp[m][n]
+
+
+str1 = input("Enter first string: ")
+str2 = input("Enter second string: ")
+
+print("Length  LCS:", lcs_length(str1, str2))
